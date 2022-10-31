42192c8e67a9f97d6f8ce3c8be68af9b6eb6ed94
Christoff <stoffel1997@gmail.com>
2022-10-31 19:54:59 +0200

    2d integration testing added

--- src/core/integrate.rs
+++ src/core/integrate.rs
@@ -1,4 +1,6 @@
-use std::{collections::BTreeSet, ops::Mul};
+use std::{cmp::max, collections::BTreeSet, ops::Mul};
+
+use ordered_float::OrderedFloat;
 
 use crate::errors::IntegError;
 
@@ -98,16 +100,106 @@ pub fn gauss_kronrod_quadrature(
     Err(IntegError::ConvergenceError)
 }
 
-pub fn gauss_kronrod_quadrature_triangle(f: impl Fn([f64; 2]) -> f64, triag: [[f64; 2]; 3]) -> Result<(f64, f64), IntegError> {
-    todo!()
-}
-
-fn symmetric_gauss_quadrature(
-    f: impl Fn(f64) -> f64,
+pub fn gauss_kronrod_quadrature_2d(
+    f: impl Fn([f64; 2]) -> f64,
     a: f64,
     b: f64,
-    rule: &[(f64, f64)],
-) -> f64 {
+    inner_ab_fn: impl Fn(f64) -> [f64; 2],
+    tol: f64,
+    max_iter: Option<usize>,
+) -> Result<(f64, f64), IntegError> {
+    if a == b {
+        return Ok((0f64, 0f64));
+    }
+    let gk_approx = |a: f64, b: f64| -> Result<(f64, f64), IntegError> {
+        let li = nested_symmetric_gauss_quadrature(
+            &f,
+            a,
+            b,
+            &inner_ab_fn,
+            tol / 2f64,
+            max_iter,
+            &LEGENDRE_NODES_WEIGHTS_10,
+        )?;
+        let ki = nested_symmetric_gauss_quadrature(
+            &f,
+            a,
+            b,
+            &inner_ab_fn,
+            tol / 2f64,
+            max_iter,
+            &KRONROD_NODES_WEIGHTS_21,
+        )?;
+        Ok((
+            ki.0,
+            (li.0 - ki.0).abs() + max(OrderedFloat(li.1), OrderedFloat(ki.1)).0,
+        ))
+    };
+    let (val, mut accu_err) = gk_approx(a, b)?;
+    let mut approx_intvls: BTreeSet<ApproxInterval> = BTreeSet::new();
+    approx_intvls.insert(ApproxInterval {
+        err: accu_err,
+        val,
+        a,
+        b,
+    });
+    for _ in 0..max_iter.unwrap_or(usize::MAX) {
+        if accu_err.is_nan() {
+            return Err(IntegError::NaNError);
+        } else if accu_err < tol {
+            return Ok((approx_intvls.into_iter().map(|v| v.val).sum(), accu_err));
+        }
+        match approx_intvls.iter().last() {
+            Some(intvl) => {
+                let intvl = intvl.clone();
+                if intvl.a != intvl.b {
+                    accu_err -= intvl.err;
+                    let m = (intvl.a + intvl.b) / 2f64;
+                    let left = gk_approx(intvl.a, m)?;
+                    let right = gk_approx(m, intvl.b)?;
+                    accu_err += left.1 + right.1;
+                    approx_intvls.insert(ApproxInterval {
+                        err: left.1,
+                        val: left.0,
+                        a: intvl.a,
+                        b: m,
+                    });
+                    approx_intvls.insert(ApproxInterval {
+                        err: right.1,
+                        val: right.0,
+                        a: m,
+                        b: intvl.b,
+                    });
+                }
+                approx_intvls.remove(&intvl);
+            }
+            None => break,
+        }
+    }
+    Err(IntegError::ConvergenceError)
+}
+
+pub fn gauss_kronrod_quadrature_triangle(
+    f: impl Fn([f64; 2]) -> f64,
+    triag: [[f64; 2]; 3],
+    tol: f64,
+    max_iter: Option<usize>,
+) -> Result<(f64, f64), IntegError> {
+    let c = (triag[1][0] * (triag[2][1] - triag[0][1])
+        + triag[0][0] * (triag[1][1] - triag[2][1])
+        + triag[2][0] * (triag[0][1] - triag[1][1]))
+        .abs();
+
+    let f = |u: [f64; 2]| {
+        c * f([
+            (1f64 - u[0] - u[1]) * triag[0][0] + u[0] * triag[1][0] + u[1] * triag[2][0],
+            (1f64 - u[0] - u[1]) * triag[0][1] + u[0] * triag[1][1] + u[1] * triag[2][1],
+        ])
+    };
+    gauss_kronrod_quadrature_2d(f, 0f64, 1f64, |u1| [0f64, 1f64 - u1], tol, max_iter)
+}
+
+fn symmetric_gauss_quadrature(f: impl Fn(f64) -> f64, a: f64, b: f64, rule: &[(f64, f64)]) -> f64 {
     (b - a) / 2f64
         * unit_symmetric_gauss_quadrature(|x| f(x * (b - a) / 2f64 + (a + b) / 2f64), rule)
 }
@@ -127,4 +219,41 @@ fn unit_symmetric_gauss_quadrature(f: impl Fn(f64) -> f64, rule: &[(f64, f64)]) 
         s += w * (f(-n) + f(n));
     }
     s
+}
+
+fn nested_symmetric_gauss_quadrature(
+    f: impl Fn([f64; 2]) -> f64,
+    a: f64,
+    b: f64,
+    inner_ab_fn: impl Fn(f64) -> [f64; 2],
+    tol: f64,
+    max_iter: Option<usize>,
+    rule: &[(f64, f64)],
+) -> Result<(f64, f64), IntegError> {
+    let outer_denormalizer = |x: f64| x * (b - a) / 2f64 + (a + b) / 2f64;
+    let outer_unit_f = |xy: [f64; 2]| f([outer_denormalizer(xy[0]), xy[1]]);
+    let mut s = 0f64;
+    let mut accu_err = 0f64;
+    if rule.len() == 0 {
+        return Ok((s, accu_err));
+    }
+    let i = if rule[0].0 == 0f64 {
+        let [ia, ib] = inner_ab_fn(outer_denormalizer(rule[0].0));
+        let res =
+            gauss_kronrod_quadrature(|y| outer_unit_f([rule[0].0, y]), ia, ib, tol, max_iter)?;
+        s += rule[0].1 * res.0;
+        accu_err += rule[0].1 * res.1;
+        1
+    } else {
+        0
+    };
+    for &(n, w) in &rule[i..] {
+        let [ia, ib] = inner_ab_fn(outer_denormalizer(-n));
+        let nres = gauss_kronrod_quadrature(|y| outer_unit_f([-n, y]), ia, ib, tol, max_iter)?;
+        let [ia, ib] = inner_ab_fn(outer_denormalizer(n));
+        let pres = gauss_kronrod_quadrature(|y| outer_unit_f([n, y]), ia, ib, tol, max_iter)?;
+        s += w * (nres.0 + pres.0);
+        accu_err += w * (nres.1 + pres.1);
+    }
+    Ok(((b - a) / 2f64 * s, (b - a) / 2f64 * accu_err))
 }
--- tests/core/integration.rs
+++ tests/core/integration.rs
@@ -1,7 +1,11 @@
 use std::f64::consts::PI;
 
 use approx::assert_abs_diff_eq;
-use cavint::core::integrate::gauss_kronrod_quadrature;
+use cavint::core::integrate::{
+    gauss_kronrod_quadrature, gauss_kronrod_quadrature_2d, gauss_kronrod_quadrature_triangle,
+};
+
+/* One Dimensional Integration Tests */
 
 fn test_fn(
     f: impl Fn(f64) -> f64,
@@ -19,12 +23,12 @@ fn test_fn(
 
 #[test]
 fn constant() {
-    test_fn(|_| 4f64, -1f64, 5f64, 1e-9, Some(10), 24f64)
+    test_fn(|_| 4f64, -1f64, 5f64, 1e-9, Some(1), 24f64)
 }
 
 #[test]
 fn linear() {
-    test_fn(|x| x + 2f64, 1f64, -1f64, 1e-9, Some(10), -4f64)
+    test_fn(|x| x + 2f64, 1f64, -1f64, 1e-9, Some(1), -4f64)
 }
 
 #[test]
@@ -47,4 +51,142 @@ fn iter_restriction() {
         Some(1)
     )
     .is_err(),)
+}
+
+/* Two Dimensional Itegration Tests */
+
+fn test_fn_2d(
+    f: impl Fn([f64; 2]) -> f64,
+    a: f64,
+    b: f64,
+    inner_ab_fn: impl Fn(f64) -> [f64; 2],
+    tol: f64,
+    max_iter: Option<usize>,
+    true_res: f64,
+) {
+    let (res, err) = gauss_kronrod_quadrature_2d(f, a, b, inner_ab_fn, tol, max_iter).unwrap();
+
+    assert!(err < tol);
+    assert_abs_diff_eq!(res, true_res, epsilon = tol);
+}
+
+#[test]
+fn prismatoid_bot() {
+    let f = |[x, y]: [f64; 2]| -x - y + 8f64;
+    let h = |[x, y]: [f64; 2]| [0.8 * x - 0.2 * y + 1.6, -0.4 * x + 0.6 * y + 3.2];
+    test_fn_2d(
+        |xy| f(h(xy)),
+        0f64,
+        4f64,
+        |_| [0f64, 4f64],
+        1e-11,
+        Some(100),
+        25.6,
+    )
+}
+
+#[test]
+fn prismatoid_top() {
+    let f = |[x, y]: [f64; 2]| (-x - y + 8f64) * (5f64 / 2f64);
+    test_fn_2d(
+        f,
+        0.8,
+        4.8,
+        |x| [
+            3.2 + if x < 1.6 {
+                -3f64 * (x - 1.6)
+            } else {
+                -0.5 * (x - 1.6)
+            },
+            4f64 + if x < 4f64 {
+                -0.5 * (x - 4f64)
+            } else {
+                -3f64 * (x - 4f64)
+            }
+        ],
+        1e-11,
+        Some(100),
+        25.6,
+    )
+}
+
+#[test]
+fn prismatoid_top2() {
+    let f = |[u, v]: [f64; 2]| 8f64 - 0.8 * u - 0.2 * v;
+    test_fn_2d(
+        f,
+        4f64,
+        6f64,
+        |_| [
+           8f64,
+           16f64
+        ],
+        1e-11,
+        Some(100),
+        25.6,
+    )
+}
+
+
+/* Triangle Integation Tests */
+
+fn test_fn_triag(
+    f: impl Fn([f64; 2]) -> f64,
+    triag: [[f64; 2]; 3],
+    tol: f64,
+    max_iter: Option<usize>,
+    true_res: f64,
+) {
+    let (res, err) = gauss_kronrod_quadrature_triangle(f, triag, tol, max_iter).unwrap();
+
+    assert!(err < tol);
+    assert_abs_diff_eq!(res, true_res, epsilon = tol);
+}
+
+fn rotate_triag_clockwise(triag: [[f64; 2]; 3], rad: f64) -> [[f64; 2]; 3] {
+    triag.map(|[x, y]| [x * rad.cos() + y * rad.sin(), y * rad.cos() - x * rad.sin()])
+}
+
+#[test]
+fn constant_triag() {
+    test_fn_triag(
+        |_| 4f64,
+        [[0f64, 0f64], [1f64, 0f64], [0f64, 1f64]],
+        1e-9,
+        Some(10),
+        2f64,
+    )
+}
+
+#[test]
+fn rotated_triag() {
+    test_fn_triag(
+        |_| 4f64,
+        rotate_triag_clockwise([[0f64, 0f64], [1f64, 0f64], [0f64, 1f64]], 1.1),
+        1e-9,
+        Some(10),
+        2f64,
+    )
+}
+
+#[test]
+fn linear_x_triag() {
+    test_fn_triag(
+        |[x, _]| x,
+        [[-1f64, 0f64], [1f64, 0f64], [0f64, 1f64]],
+        1e-9,
+        Some(10),
+        0f64,
+    )
+}
+
+#[test]
+fn linear_y_triag() {
+    test_fn_triag(
+        |[_, y]| y,
+        [[-1f64, 0f64], [1f64, 0f64], [0f64, 1f64]],
+        1e-9,
+        Some(10),
+        1f64 / 3f64,
+    )
 }
